97217cde7538cffec8982d830aa52b30c8730c85
Yvan-Dev0 <ymaribbay@thurstonsoft.com>
2026-06-24 09:52:55 -0700

    Register SignalR in DI Container

--- CheckerGame/Program.cs
+++ CheckerGame/Program.cs
@@ -5,18 +5,20 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 // Register DB Connection String
 builder.Services.AddDbContext<GameDBContex>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Register SignalR
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
 app.MapHub<GameHub>("/gamehub");
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
