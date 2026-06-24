bff8d47a36f46dca8f88b815d3257ecefdcae865
Yvan-Dev0 <ymaribbay@thurstonsoft.com>
2026-06-24 11:10:43 -0700

    Update GameHub

--- CheckerGame/Hubs/GameHub.cs
+++ CheckerGame/Hubs/GameHub.cs
@@ -14,5 +14,16 @@ namespace CheckerGame.Hubs
             await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
             await Clients.Group(gameId).SendAsync("PlayerJoined", Context.ConnectionId);
         }
+
+        public async Task MakeMove(string gameId, int fromX, int fromY, int toX, int toY)
+        {
+            await Clients.Group(gameId).SendAsync("GameUpdated", new
+            {
+                fromX,
+                fromY,
+                toX,
+                toY
+            });
+        }
     }
 }
