a5ade6c6f0f7ad7d7252893cd18d8cbb779d5f0a
Yvan-Dev0 <ymaribbay@thurstonsoft.com>
2026-06-26 09:57:59 -0700

    add login form

---
+++ checkergame-ui/src/components/LoginForm.jsx
@@ -0,0 +1,37 @@
+import { use, useState } from "react";
+import { login } from "../api/auth";
+
+export default function LoginForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await login(username, password);
+            localStorage.setItem("token", res.data.token);
+            alert('Welcome ${res.data.username}');
+        } catch (error) {
+            alert("Login failed: " + error.response?.data);
+        }
+    };
+
+    return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
